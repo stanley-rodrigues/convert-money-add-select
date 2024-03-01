4a03972e00a0ec302fa1ae8b07c2d8d76dc9c035
Stanley-Rodrigues <stanleyrodriguessilva@gmail.com>
2024-03-01 19:28:36 -0300

    add coments in code

--- scripts.js
+++ scripts.js
@@ -1,18 +1,32 @@
-const convertButton = document.querySelector('.convert-button')
-const segundoSelect = document.querySelector('.segundo-select')
-const primeiroSelect = document.querySelector('.primeiro-select')
+/* Para adicionar moedas ao select de cima será feito o mapeamento de tudo igual é feito para o segundo, irá mapear o select, a imagem da moeda o nome... */
+
+const convertButton = document.querySelector('.convert-button') // mapeia o botão que vai converter
+const segundoSelect = document.querySelector('.segundo-select') // mapeia o segundo select
+const primeiroSelect = document.querySelector('.primeiro-select') // mapeia o primeiro select
 
 
 function convertValues() {
-  const inputCurrencyValue = document.querySelector('.input-currency').value
+  const inputCurrencyValue = document.querySelector('.input-currency').value // mapeia o valor digitado no input
   const currencyValueToConvert = document.querySelector(
     '.currency-value-to-convert',
-  ) // Valor em Real
-  const currencyValueConverted = document.querySelector('.currency-value') // Outras moedas
+  ) // mapeia Valor da primeira moeda
+  const currencyValueConverted = document.querySelector('.currency-value') // mapeia o valor da segunda moeda
 
   const dolarToday = 5.2
-  
 
+
+  /*Para fazer a conversão precisa satisfazer as duas condições quando o primeiro select for  dolar e o segundo for real  irá converter de dolar para real */
+
+  if (primeiroSelect.value == 'dolar' && segundoSelect.value == 'real') {
+
+    currencyValueConverted.innerHTML = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(inputCurrencyValue * dolarToday)
+
+  }
+
+  /*Quando o primeiro select for real e o segundo for dolar irá converter real para dolar */
 
   if (segundoSelect.value == 'dolar' && primeiroSelect.value == 'real') {
     currencyValueConverted.innerHTML = new Intl.NumberFormat('en-US', {
@@ -22,30 +36,19 @@ function convertValues() {
 
   }
 
-  if (primeiroSelect.value == 'dolar' && segundoSelect.value == 'real') {
-
-    currencyValueConverted.innerHTML = new Intl.NumberFormat('pt-BR', {
-      style: 'currency',
-      currency: 'BRL',
-    }).format(inputCurrencyValue * 4.78) // valor de 4.78 equivale a um dolar com cada moeda o real terá valor diferente 
-    
-  }
+  /*CASO DESEJE CONVERTER DOLAR PARA EURO | EURO PARA DOLAR OU QUALQUER OUTRA MOEDA USAR A MESMA LÓGICA DE CIMA BASTA COPIAR E COLAR E ALTERAR PARA A MOEDA DESEJADA*/
 
 
+  // Essa função é responsável por formatar o texto da moeda do primeiro select, quando for dolar irá colocoar o $ e quando for real R$, para adicionar outras moedas basta copiar e colar e alterar para a moeda que deseja
+  function formatCurrency() {
 
-  // currencyValueToConvert.innerHTML = new Intl.NumberFormat('en-BR', {
-  //   style: 'currency',
-  //   currency: 'BRL',
-  // }).format(inputCurrencyValue)
-  
-function formatCurrency() {
-
-  switch (primeiroSelect.value) {
+    switch (primeiroSelect.value) {
       case 'dolar':
         currencyValueToConvert.innerHTML = new Intl.NumberFormat('en-US', {
           style: 'currency',
           currency: 'USD',
         }).format(inputCurrencyValue);
+
 
         break;
       case 'real':
@@ -55,11 +58,17 @@ function formatCurrency() {
         }).format(inputCurrencyValue);
         break;
 
+
+
+
+
       default:
         break;
     }
-}
- formatCurrency()
+  }
+  formatCurrency()
+
+
 }
 
 
@@ -67,12 +76,14 @@ function formatCurrency() {
 
 
 function changeCurrency() {
-  const nomeMoeda2 = document.getElementById('nome-moeda-select-2')
-  const nomeMoeda1 = document.getElementById('nome-moeda-select-1')
-  const currencyImage = document.querySelector('.currency-img')
-  const imagemPrimeiroSelect = document.querySelector('.imagem-primeiro-select')
-  
+  const nomeMoeda2 = document.getElementById('nome-moeda-select-2') // mapeia o nome da segunda moeda
+  const nomeMoeda1 = document.getElementById('nome-moeda-select-1') // mapeia o nome da primeira moeda
+  const currencyImage = document.querySelector('.currency-img') // mapeia a segunda imagem
+  const imagemPrimeiroSelect = document.querySelector('.imagem-primeiro-select') // mapeia a primeira imagem
 
+
+
+  //aqui a condicional precisa ser feita para o primeiro select e para o segundo, assim, irá trocar a imagem e o nome nos dois select
   if (segundoSelect.value == 'real') {
     nomeMoeda2.innerHTML = 'Real Brasileiro'
     currencyImage.src = './assets/real.png'
@@ -93,9 +104,11 @@ function changeCurrency() {
     imagemPrimeiroSelect.src = './assets/dolar.png'
   }
 
+  
+
 
   convertValues()
 }
-primeiroSelect.addEventListener('change', changeCurrency)
-segundoSelect.addEventListener('change', changeCurrency)
-convertButton.addEventListener('click', convertValues)
+primeiroSelect.addEventListener('change', changeCurrency) // aqui pega o evento de mudança do primeiro select
+segundoSelect.addEventListener('change', changeCurrency) // aqui pega o evento de mudança do segundo select
+convertButton.addEventListener('click', convertValues) //aqui pega o evento de clique do botão.
